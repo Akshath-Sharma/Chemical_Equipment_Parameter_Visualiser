b465d6e08fcfd3c37a5491b01af4659c86cd0981
Akshath Sharma <akshath.sharma21@gmail.com>
2026-02-10 02:05:42 +0530

    Update settings.py

--- backend/chemical_visualizer/settings.py
+++ backend/chemical_visualizer/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG_MODE', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
