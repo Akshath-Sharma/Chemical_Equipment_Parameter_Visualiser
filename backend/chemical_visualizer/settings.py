d3d4dbb4d3cbd2022c972ea31dfc346931615a17
Akshath Sharma <akshath.sharma21@gmail.com>
2026-02-10 17:18:37 +0530

    Update settings.py

--- backend/chemical_visualizer/settings.py
+++ backend/chemical_visualizer/settings.py
@@ -143,6 +143,9 @@ MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000","https://chemical-equipment-parameter-visual-phi.vercel.app","https://chemical-equipment-paramete-git-9dfc3a-akshath-sharmas-projects.vercel.app"]
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.vercel\.app$",
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
